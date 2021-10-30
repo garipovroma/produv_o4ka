80ab05135b485417747f036a243c46264b3ddaf4
Rai-Fox <rai_fox@mail.ru>
2021-10-30 03:07:26 +0300

    reduced ts added

--- utils/model.py
+++ utils/model.py
@@ -7,11 +7,10 @@ from sklearn.model_selection import KFold
 with open('config.yaml') as f:
     config = yaml.load(f, yaml.FullLoader)
 
-
+    
 def save_catboost_models(model_tst, model_C):
     model_tst.save_model(config['model_tst_path'])
     model_C.save_model(config['model_C_path'])
-
 
 def get_catboost_models():
     model_tst = CatBoostRegressor()
@@ -20,17 +19,14 @@ def get_catboost_models():
     model_C.load_model(config['model_C_path'])
     return model_tst, model_C
 
-
 def predict(test_data, model_tst, model_C):
     y_pred = test_data[['NPLV']]
     y_pred['TST'] = model_tst.predict(test_data)
     y_pred['C'] = model_C.predict(test_data)
     return y_pred
 
-
 def save_to_csv(y):
     y.to_csv(config['submission_path'])
-
 
 def metric(answers, user_csv):
     delta_c = np.abs(np.array(answers['C']) - np.array(user_csv['C']))
@@ -43,16 +39,15 @@ def metric(answers, user_csv):
 
     return np.sum(hit_rate_c + hit_rate_t) / 2 / N
 
-
-def cross_val(X: pd.DataFrame, y: pd.DataFrame, model_tst, model_C, metric, n: int = 5) -> list:
-    kf = KFold(n_splits=n)
+def cross_val(X: pd.DataFrame, y: pd.DataFrame, model_tst, model_C, n: int = 5) -> list:
+    kf = KFold(n_splits=n, shuffle=True, random_state=config['random_state'])
     res = []
     for train_index, test_index in kf.split(X):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
         model_tst.fit(X_train, y_train['TST'])
-        model_C.fit(X_train, y_train['C'].fillna(y_train['C'].mean()))
+        model_C.fit(X_train, y_train['C'])
 
         pred_train = predict(X_train, model_tst, model_C)
         pred_test = predict(X_test, model_tst, model_C)
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -8,7 +8,8 @@ with open('config.yaml') as f:
 files = config['files']
 
 def read_target_train():
-    return pd.read_csv(config['data_path'] + 'target_train.csv')
+    target = pd.read_csv(config['data_path'] + 'target_train.csv')
+    return target.fillna(target.mean())
 
 def read_data(suffix):
     data = {}
@@ -16,11 +17,33 @@ def read_data(suffix):
         data[file] = pd.read_csv(f'{config["data_path"]}{file}_{suffix}.csv')
     data['chronom'].drop(['Unnamed: 0'], axis = 1, inplace = True)
     return data
-
+    
 def preprocess_data(data):
+    data['reduced_gas'] = ts_preproc(data['reduced_gas'], 'Time')
+    data['reduced_produv'] = ts_preproc(data['reduced_produv'], 'SEC')
     X = data['chugun'].drop(['DATA_ZAMERA'], axis = 1)
     for file in files:
         if file == 'chugun':
             continue
-        X = X.merge(data[file].groupby('NPLV').mean(), on = 'NPLV', suffixes=('',f'_{file}'))
+        aggregated_data = data[file].groupby('NPLV').agg([np.mean, np.sum, np.min, np.max])
+        aggregated_data.columns = list(map(lambda x: x[0] + "_" + x[1], aggregated_data.columns))
+        X = X.merge(aggregated_data, on = 'NPLV', suffixes=('',f'_{file}'))
     return X
+
+def ts_preproc(df, time_col):
+    df[time_col] = pd.to_datetime(df[time_col])
+    df = df.set_index(time_col)
+    return df
+
+def reduse_to_produv(ts_data:pd.DataFrame, chronom:pd.DataFrame) -> pd.DataFrame:
+    new_ts_data = pd.DataFrame()
+    for NPLV in ts_data.NPLV.unique():
+        curr_new_ts = ts_data[ts_data['NPLV'] == NPLV][(ts_data[ts_data['NPLV'] == NPLV].index > 
+                                                          (chronom[(chronom['NPLV'] == NPLV) & 
+                                                                   (chronom['NOP'] == 'Продувка')]['VR_NACH'].values[0])) & 
+                                                         (ts_data[ts_data['NPLV'] == NPLV].index < 
+                                                          (chronom[(chronom['NPLV'] == NPLV) & 
+                                                                   (chronom['NOP'] == 'Продувка')]['VR_KON'].values[0]))]
+        new_ts_data = pd.concat((new_ts_data,curr_new_ts))
+        
+    return new_ts_data
