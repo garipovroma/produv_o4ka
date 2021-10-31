2698a2b65599257837393527f596cd1965609f42
garipovroma <devilgar@gmail.com>
2021-10-31 06:26:22 +0000

    predlast submit

--- make_zip.sh
+++ make_zip.sh
@@ -1,2 +1,2 @@
-zip solution.zip first_submission.csv predict.ipynb preprocess.ipynb train.ipynb -r utils/ 
+zip solution.zip first_submission.csv predict.ipynb preprocess.ipynb train.ipynb chronom_duration.ipynb chronom_ts.ipynb clustering.ipynb config.yaml model_C model_tst plavki.ipynb README.md reduce_ts.ipynb requirements.txt sip_lom_preproc.ipynb  -r utils/ -r data_chronom/ -r utils/
 
